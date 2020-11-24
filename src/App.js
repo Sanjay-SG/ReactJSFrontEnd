ef06b48b8f3fb08bd92edb00fac548d91d20edb3
Sanjay-SG <sanjay.s.gunagi@gmail.com>
2020-11-24 21:38:36 +0530

    Used reactstrap and react tostify

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react"
 
+import {Button} from "reactstrap"
+import {ToastContainer, toast} from "react-toastify"
 const App = () =>
 {
+    const btnHandle = () =>
+    {
+        toast("This a message..")
+    }
     return(
         <>
-            <h1>App</h1>
+        <ToastContainer/>
+            <h1>Application</h1>
+            <Button color="warning" onClick={btnHandle}>React Button</Button>
         </>
     )
 }
